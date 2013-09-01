00948ae3d2e1ed2853f92fdaf910f5848663dc0d
itmammoth <itmammoth@gmail.com>
2013-09-01 19:44:17 +0700

    チューニング１完了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,7 +2,7 @@ class BooksController < ApplicationController
   # GET /books
   # GET /books.json
   def index
-    @books = Book.all
+    @books = Book.with_author
 
     respond_to do |format|
       format.html # index.html.erb
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,8 @@
 class Book < ActiveRecord::Base
   belongs_to :author
   attr_accessible :author_id, :description, :title
+
+  scope :with_author, -> do
+    includes(:author)
+  end
 end
